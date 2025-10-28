141eef644f7c3b06c4b7ae745fd395c8dbdf63fd
darknessofmine <doessmth@gmail.com>
2025-10-28 22:20:53 +0500

    frontend: created LoginPage, HomePage components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,18 @@ import type React from 'react' ;
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import './App.css'
+import { HomePage } from './components/home-page/HomePage';
+import { LoginPage } from './components/login-page/LoginPage';
 import { MainLayout } from './components/main-layout/MainLayout';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/auth/login' element={<> loginPage </>} />
+        <Route path='/auth/login' element={<LoginPage/>} />
 
         <Route path='/' element={<MainLayout/>}>
-          <Route path='/' element={<>homePage</>}/>
+          <Route path='/' element={<HomePage/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
