73389667a7d148a484a1afc2675f5e2a732a16d1
darknessofmine <doessmth@gmail.com>
2025-10-28 22:14:14 +0500

    frontend: added BrowserRouter, created mainLayout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,21 @@
+import type React from 'react' ;
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import './App.css'
+import { MainLayout } from './components/main-layout/MainLayout';
 
-function App() {
-
+const App: React.FC = () => {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/auth/login' element={<> loginPage </>} />
+
+        <Route path='/' element={<MainLayout/>}>
+          <Route path='/' element={<>homePage</>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/main-layout/MainLayout.tsx
@@ -0,0 +1,12 @@
+import type React from "react";
+import { Outlet } from "react-router-dom";
+
+
+export const MainLayout: React.FC = () => {
+    return (
+        <>
+            <>navMenu</>
+            <Outlet/>
+        </>
+    )
+}
