cafdf22f7a6f29f38e92211d8e493f259c5b6d71
darknessofmine <doessmth@gmail.com>
2025-10-29 18:50:47 +0500

    frontend: LoginPage - added login-form html part

--- frontend/src/components/login-page/LoginPage.tsx
+++ frontend/src/components/login-page/LoginPage.tsx
@@ -1,8 +1,63 @@
 import React from 'react';
 
+import styles from './LoginPage.module.css'
+
 
 export const LoginPage: React.FC = () => {
+    const [loginForm, setLoginForm] = React.useState({
+        username: '',
+        password: '',
+    });
+
+    const handleFormChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setLoginForm({
+            ...loginForm,
+            [e.target.name]: e.target.value,
+        });
+    }
+
+    const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+    };
+
     return (
-        <div>login-page</div>
+        <main>
+            <div className={styles.loginPageContainer} >
+                <div className={styles.loginFormContainer} >
+                    <form
+                        id="loginForm"
+                        className={styles.loginForm}
+                        onSubmit={handleFormSubmit}
+                    >
+                        <div className={styles.loginFormContent} >
+                            <div className={styles.usernameFieldContainer} >
+                                <input
+                                    type="text"
+                                    name="username"
+                                    className={styles.usernameField}
+                                    placeholder="username"
+                                    onChange={handleFormChange}
+                                />
+                            </div>
+                            <div className={styles.passwordFieldContainer} >
+                                <input
+                                    type="password"
+                                    name="password"
+                                    className={styles.passwordField}
+                                    placeholder="password"
+                                    onChange={handleFormChange}
+                                />
+                            </div>
+
+                            <div className={styles.buttonContainer}>
+                                <button type="submit" form="loginForm">
+                                    Enter
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </main>
     );
 }
